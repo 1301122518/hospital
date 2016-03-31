ac5fc3722e0bf34c6c314587a66196b88c8fb1a9
someLiang <viso.liang@qq.com>
2016-03-31 18:01:58 +0800

    1.添加jpa文件，使项目可以与数据库连接。 2.添加病人model和病人的基本crud管理patientRepository。 3.Spring注入有问题，需要修改。

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,48 @@
 package controllers;
 
-import play.*;
+import models.Patient;
+import models.PatientRepository;
 import play.mvc.*;
+import play.*;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
 import views.html.*;
 
+/**
+ * The main set of web services.
+ */
+@Named
+@Singleton
 public class Application extends Controller {
+
+    private final PatientRepository patientRepository;
+
+    // We are using constructor injection to receive a repository to support our desire for immutability.
+    @Inject
+    public Application(final PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
 
     public Result show(String name) {
         return ok(views.html.hello.render(name));
     }
 
     public Result index() {
-        return ok(views.html.index.render("your application is ready"));
+        final Patient patient = new Patient();
+        patient.id = 1L;
+        patient.name = "Ling";
+
+        final Patient savedPatient = patientRepository.save(patient);
+
+
+        final Patient retrievedPatient = patientRepository.findOne(savedPatient.id);
+
+        // Deliver the index page with a message showing the id that was generated.
+
+        return ok(views.html.index.render("Found id: " + retrievedPatient.id + retrievedPatient.name + " of person/people"));
     }
 }
--- build.sbt
+++ build.sbt
@@ -6,11 +6,23 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
 scalaVersion := "2.11.6"
 
+//playJavaSettings
+
+//ebeanEnabled := false
+
 libraryDependencies ++= Seq(
   javaJdbc,
   cache,
   javaWs,
-  "mysql" % "mysql-connector-java" % "5.1.18"
+  javaCore,
+  javaJpa,
+  "mysql" % "mysql-connector-java" % "5.1.18",
+  "org.springframework" % "spring-context" % "4.1.6.RELEASE",
+  "javax.inject" % "javax.inject" % "1",
+  "org.springframework.data" % "spring-data-jpa" % "1.3.2.RELEASE",
+  "org.springframework" % "spring-expression" % "3.2.2.RELEASE",
+  "org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
+  "org.mockito" % "mockito-core" % "1.9.5" % "test"
 )
 
 
