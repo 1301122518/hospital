fbe381359827bfacaa622c4408894a3cd875d974
Rita Liang <viso.liang@qq.com>
2016-04-30 00:57:43 +0800

    将备份SQL修改为真正视图内容。

--- hospital.sql
+++ hospital.sql
@@ -1,133 +1,95 @@
--- MySQL dump 10.13  Distrib 5.6.25, for osx10.8 (x86_64)
---
--- Host: localhost    Database: hospital
--- ------------------------------------------------------
--- Server version	5.6.25
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : hospital
+Source Server Version : 50628
+Source Host           : localhost:3306
+Source Database       : hospital
 
---
--- Table structure for table `Patient`
---
-USE hospital;
+Target Server Type    : MYSQL
+Target Server Version : 50628
+File Encoding         : 65001
 
-DROP TABLE IF EXISTS `Patient`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Patient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `idCardNo` varchar(18) DEFAULT NULL,
-  `name` varchar(10) DEFAULT NULL,
-  `gender` varchar(2) DEFAULT NULL,
-  `age` tinyint(4) DEFAULT NULL,
+Date: 2016-04-30 00:56:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for application
+-- ----------------------------
+DROP TABLE IF EXISTS `application`;
+CREATE TABLE `application` (
+  `id` int(11) NOT NULL,
+  `idCardNo` varchar(20) DEFAULT NULL,
+  `applyItem` varchar(20) DEFAULT NULL,
+  `signDoctor` varchar(20) DEFAULT NULL,
+  `applyDepartment` varchar(20) DEFAULT NULL,
+  `applyTime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `examAddress` varchar(56) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Patient`
---
-
-LOCK TABLES `Patient` WRITE;
-/*!40000 ALTER TABLE `Patient` DISABLE KEYS */;
-INSERT INTO `Patient` VALUES (1,'51018419880821006X','梁爽','女',27),(2,'51018419880821012X','陈爽','女',20);
-/*!40000 ALTER TABLE `Patient` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Person`
---
-
-DROP TABLE IF EXISTS `Person`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `PatientPerson` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(10) DEFAULT NULL,
-  `surname` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Person`
---
-
-LOCK TABLES `Person` WRITE;
-/*!40000 ALTER TABLE `Person` DISABLE KEYS */;
-INSERT INTO `Person` VALUES (1,'Ling','Liang'),(2,'Ling','Liang');
-/*!40000 ALTER TABLE `Person` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `department`
---
-
-DROP TABLE IF EXISTS `department`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `department` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL,
-  `address` varchar(56) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `department`
---
-
-LOCK TABLES `department` WRITE;
-/*!40000 ALTER TABLE `department` DISABLE KEYS */;
-INSERT INTO `department` VALUES (1,'放射科','A区一楼门诊部外科2诊断室'),(2,'化验室',NULL);
-/*!40000 ALTER TABLE `department` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `patient_department`
---
-
-DROP TABLE IF EXISTS `patient_department`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `patient_department` (
-  `patient_id_card_no` varchar(18) DEFAULT NULL,
-  `department_id` int(11) DEFAULT NULL,
-  `patient_id` int(11) DEFAULT NULL,
-  KEY `FK2A0008CC14A2A29E` (`department_id`),
-  KEY `FK2A0008CC633DDFF6` (`patient_id`),
-  CONSTRAINT `FK2A0008CC14A2A29E` FOREIGN KEY (`department_id`) REFERENCES `Department` (`id`),
-  CONSTRAINT `FK2A0008CC633DDFF6` FOREIGN KEY (`patient_id`) REFERENCES `Patient` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `patient_department`
---
+-- ----------------------------
+-- Records of application
+-- ----------------------------
+INSERT INTO `application` VALUES ('1', '51018419880821006X', '胸部正位', '吴静', '体检部', '2016-01-05 08:51:00', 'C区1楼放射科');
 
-LOCK TABLES `patient_department` WRITE;
-/*!40000 ALTER TABLE `patient_department` DISABLE KEYS */;
-INSERT INTO `patient_department` VALUES ('51018419880821006X',1,1),('51018419880821006X',2,1);
-/*!40000 ALTER TABLE `patient_department` ENABLE KEYS */;
-UNLOCK TABLES;
+-- ----------------------------
+-- Table structure for examination
+-- ----------------------------
+DROP TABLE IF EXISTS `examination`;
+CREATE TABLE `examination` (
+  `id` int(11) NOT NULL,
+  `idCardNo` varchar(255) DEFAULT NULL,
+  `examDepartment` varchar(30) DEFAULT NULL,
+  `examAddress` varchar(255) DEFAULT NULL,
+  `examItem` varchar(30) DEFAULT NULL,
+  `admin` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `play_evolutions`
---
+-- ----------------------------
+-- Records of examination
+-- ----------------------------
+INSERT INTO `examination` VALUES ('1', '51018419880821006X', '体检部-外科检查', 'A区一楼门诊部外科2诊室', '外科检查', '陈长');
+INSERT INTO `examination` VALUES ('2', '51018419880821006X', '体检部-内科检查', 'A区一楼门诊部外科1诊室', '内科检查', '');
+INSERT INTO `examination` VALUES ('3', '51018419880821006X', '体检部-眼耳鼻喉检查室', 'A区一楼门诊部', '眼科检查', null);
+INSERT INTO `examination` VALUES ('4', '51018419880821006X', '体检部-口腔检查检查室', 'A区一楼门诊部', '口腔检查', null);
+INSERT INTO `examination` VALUES ('5', '51018419880821006X', '妇科检查', 'C区1楼体检部', '妇检', null);
 
+-- ----------------------------
+-- Table structure for person
+-- ----------------------------
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idCardNo` varchar(20) DEFAULT NULL,
+  `organisation` varchar(36) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `marryStatus` varchar(20) DEFAULT NULL,
+  `examID` varchar(36) DEFAULT NULL,
+  `examImage` varchar(36) DEFAULT NULL,
+  `applyID` varchar(36) DEFAULT NULL,
+  `applyImage` varchar(36) NOT NULL,
+  `cost` decimal(10,0) DEFAULT NULL,
+  `allCost` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`id`,`applyImage`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of person
+-- ----------------------------
+INSERT INTO `person` VALUES ('1', '51018419880821006X', '四川化工职业技术学院', '陈张', '男', '34', '15228461257', '已婚', '1600234012', null, '1600234012', '', '199', '3000');
+INSERT INTO `person` VALUES ('2', 'Ling', 'Liang', null, null, null, null, null, null, null, null, '', null, null);
+
+-- ----------------------------
+-- Table structure for play_evolutions
+-- ----------------------------
 DROP TABLE IF EXISTS `play_evolutions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `play_evolutions` (
   `id` int(11) NOT NULL,
   `hash` varchar(255) NOT NULL,
@@ -138,25 +100,8 @@ CREATE TABLE `play_evolutions` (
   `last_problem` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `play_evolutions`
---
-
-LOCK TABLES `play_evolutions` WRITE;
-/*!40000 ALTER TABLE `play_evolutions` DISABLE KEYS */;
-INSERT INTO `play_evolutions` VALUES (1,'da39a3ee5e6b4b0d3255bfef95601890afd80709','2016-03-30 14:04:10','','','applied','');
-/*!40000 ALTER TABLE `play_evolutions` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-04-15 10:28:57
+-- ----------------------------
+-- Records of play_evolutions
+-- ----------------------------
+INSERT INTO `play_evolutions` VALUES ('1', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '2016-03-30 14:04:10', '', '', 'applied', '');
